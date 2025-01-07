d7e908ad3edd0ba80243c9bc47fc49035814484a
George Kwabena Asiedu <90930379+george-asiedu@users.noreply.github.com>
2025-01-07 19:31:31 +0000

    about me component added

--- src/app/portfolio/portfolio.module.ts
+++ src/app/portfolio/portfolio.module.ts
@@ -5,11 +5,13 @@ import { PortfolioRoutingModule } from './portfolio-routing.module';
 import { MainPageComponent } from './main-page/main-page.component';
 import { HomeComponent } from './home/home.component';
 import { SharedModule } from '../shared/shared.module';
+import { AboutMeComponent } from './about-me/about-me.component';
 
 @NgModule({
   declarations: [
     MainPageComponent,
-    HomeComponent
+    HomeComponent,
+    AboutMeComponent
   ],
   imports: [
     CommonModule,
