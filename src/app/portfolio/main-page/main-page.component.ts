df7009af053f882eb7d9f4d80ee0782bb074baaa
George Kwabena Asiedu <90930379+george-asiedu@users.noreply.github.com>
2025-01-07 17:16:21 +0000

    main page component creatd

--- src/app/portfolio/main-page/main-page.component.ts
+++ src/app/portfolio/main-page/main-page.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-main-page',
   standalone: false,
-  
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.scss'
 })
