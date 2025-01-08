11e3868ee155f487f6d4e5edea0cbd30833ac84b
George Kwabena Asiedu <90930379+george-asiedu@users.noreply.github.com>
2025-01-08 15:20:08 +0000

    Footer component added

--- src/app/portfolio/portfolio.module.ts
+++ src/app/portfolio/portfolio.module.ts
@@ -6,16 +6,20 @@ import { MainPageComponent } from './main-page/main-page.component';
 import { HomeComponent } from './home/home.component';
 import { SharedModule } from '../shared/shared.module';
 import { AboutMeComponent } from './about-me/about-me.component';
+import { FooterComponent } from './footer/footer.component';
+import { RouterLink } from '@angular/router';
 
 @NgModule({
   declarations: [
     MainPageComponent,
     HomeComponent,
-    AboutMeComponent
+    AboutMeComponent,
+    FooterComponent
   ],
   imports: [
     CommonModule,
     PortfolioRoutingModule,
+    RouterLink,
     SharedModule
   ]
 })
